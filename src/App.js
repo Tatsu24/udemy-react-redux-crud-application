694d661d61e5c524b6588e166bf9d975f30e9b60
Tatsu24 <otrgb2033@gmail.com>
2020-11-25 03:15:11 +0900

    jsxファイルについて

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello, world!</h1></div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello, world!</h1>
+        <label htmlFor="bar"></label>
+        <input type="text" onClick={() => { console.log("I'm Click!") }}></input>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
