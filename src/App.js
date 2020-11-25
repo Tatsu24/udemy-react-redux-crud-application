c86c1f989da8bbfddeca41dc99608ad6623e6242
Tatsu24 <otrgb2033@gmail.com>
2020-11-25 14:24:54 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 
+// class component
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>Hello, world!</h1>
+//         <label htmlFor="bar"></label>
+//         <input type="text" onClick={() => { console.log("I'm Click!") }}></input>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Hello, world!</h1>
-        <label htmlFor="bar"></label>
-        <input type="text" onClick={() => { console.log("I'm Click!") }}></input>
-      </React.Fragment>
-    )
-  }
+const App = () => {
+  return <div>Hi!</div>
 }
 
 export default App;
