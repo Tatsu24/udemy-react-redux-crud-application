048a7eac66041b8e5f47e63bacfc0277c21a4a70
Tatsu24 <otrgb2033@gmail.com>
2020-11-25 14:45:52 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import React, { Component } from 'react';
 import React from 'react';
+import PropTypes from 'prop-types'
 
 // class component
 // class App extends Component {
-
 //   render() {
 //     return (
 //       <React.Fragment>
@@ -20,15 +20,9 @@ import React from 'react';
 
 const App = () => {
   const profiles = [
-    {
-      name: "Taro", age: 10
-    },
-    {
-      name: "Hanako", age: 5
-    },
-    {
-      name: "noName"
-    },
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "noName", },
   ]
   return (
     <React.Fragment>
@@ -36,8 +30,8 @@ const App = () => {
       <Hi />
       <div>
         {
-          profiles.map((profile, index)=>{
-            return <User name={profile.name} age={profile.age} key={index}/>
+          profiles.map((profile, index) => {
+            return <User name={profile.name} age={profile.age} key={index} />
           })
         }
       </div>
@@ -55,8 +49,12 @@ const Hi = () => {
 const User = (props) => {
   return <div>Hi! {props.name}, and {props.age} years old!</div>
 }
-User.defaultProps = {
-  age: 1
+// User.defaultProps = {
+//   age: 1
+// }
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
