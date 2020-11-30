af69a50f89ec984d7718d07a905316dede8de32e
Tatsu24 <otrgb2033@gmail.com>
2020-12-01 02:00:57 +0900

    redux-devtools-extension 追加

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css';
 import reducer from './reducers'
@@ -11,9 +12,11 @@ import EventsIndex from './components/events_index.jsx';
 import EventsNew from './components/events_new.jsx';
 import reportWebVitals from './reportWebVitals';
 
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 // thunkを使うことで
 // action の代わりに関数を返すことができる
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
